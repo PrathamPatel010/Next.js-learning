8b8b668e93c51392d20007d19f848a47a1089a93
Pratham Patel <pratham@logicubeit.com>
2024-05-18 18:28:22 +0530

    added error handling

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react'
+
+const error = () => {
+    return (
+        <div>Some error has occured</div>
+    )
+}
+
+export default error;
---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            Sorry bro!! The page you looking for is not there..
+        </div>
+    )
+}
+
+export default NotFoundPage;
