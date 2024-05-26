f2dfaf58ab0f02ea9ec01573b1881fd410a90701
PrathamPatel010 <ppratham812@gmail.com>
2024-05-26 09:58:45 +0530

    added home route link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Link from "next/link";
 
 const NotFoundPage = () => {
     return (
         <div>
             Sorry bro!! The page you looking for is not there..
+            Click below to redirect to main page.<br/>
+            <Link href={'/'} className={'bg-blue-500 p-1 rounded-sm'}>
+                Go to Home
+            </Link>
         </div>
     )
 }
